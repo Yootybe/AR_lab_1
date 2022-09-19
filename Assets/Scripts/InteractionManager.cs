c13fbad90b13c602ae119394176a2dbb460d9a82
Vasiliy V. Dmitriev <vvd17@tpu.ru>
2022-09-19 20:25:44 +0700

    lab2 without var

--- Assets/Scripts/InteractionManager.cs
+++ Assets/Scripts/InteractionManager.cs
@@ -1,6 +1,7 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
@@ -9,37 +10,154 @@ using UnityEngine.XR.ARSubsystems;
 
 public class InteractionManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _spawnedObjectPrehab;
+    [SerializeField] private GameObject[] _spawnedObjectPrehabs;
+    [SerializeField] private GameObject _targetMarkerPrehab;
+    [SerializeField] private GameObject[] _uiScreens;
+
+    // Each interaction manager state correlate to UI Screens:
+    // Default (0) - _uiScreens[0], SpawnObject(1) - _uiScreens[1] etc.
+
+    private enum InterractionManagerState { Default, SpawnObject }
+    private InterractionManagerState _currentState;
+    private UnityAction[] _stateInitializationAction;
+    private int _spawnedObjectType = -1;
 
     private ARRaycastManager _aRRaycastManager;
     private List<ARRaycastHit> _raycastHits;
+    private GameObject _targetMarker;
 
     private void Awake()
     {
         _aRRaycastManager = GetComponent<ARRaycastManager>();
         _raycastHits = new List<ARRaycastHit>();
+
+        _stateInitializationAction = new UnityAction[Enum.GetNames(typeof(InterractionManagerState)).Length];
+        _stateInitializationAction[(int)InterractionManagerState.Default] = InitializeDefaultScreen;
+        _stateInitializationAction[(int)InterractionManagerState.Default] = InitializeObjectSpawner;
     }
 
     private void Start()
     {
-                
+        // Create target marker
+        _targetMarker = Instantiate(
+            original: _targetMarkerPrehab,
+            position: Vector3.zero,
+            rotation: _targetMarkerPrehab.transform.rotation
+        );
+
+        _targetMarker.SetActive(false);
+
+        // Reset current state
+        _currentState = InterractionManagerState.Default;
+        UpdateUIScreens();
+    }
+
+    private void UpdateUIScreens()
+    {
+        // Hide every UI screen
+        foreach (GameObject uiObject in _uiScreens)
+        {
+            uiObject.SetActive(false);
+        }
+
+        // Show the UI screen for the current state
+        _uiScreens[(int)_currentState].SetActive(true);
+
+        // Call initialization function of the current UI screen
+        _stateInitializationAction[(int)_currentState]();
+    }
+
+    public void DisplayUIScreen(int screenNymber)
+    {
+        _currentState = (InterractionManagerState) screenNymber;
+        UpdateUIScreens();
     }
 
     private void Update()
     {
         if (Input.touchCount > 0)
-            ProcessFirstTouch(Input.GetTouch(0));
+        {
+            Touch touch = Input.GetTouch(0);
+            bool isOverUI = touch.position.IsPointOverUIObject();
+
+            switch (_currentState)
+            {
+                case InterractionManagerState.SpawnObject:
+                    ProcessTouchSpawnObject(touch, isOverUI);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        //ProcessFirstTouch(Input.GetTouch(0));
     }
 
-    private void ProcessFirstTouch(Touch touch)
+    private void ProcessTouchSpawnObject(Touch touch, bool overUI)
     {
+        // If none are ye selected, return
+
+        if (_spawnedObjectType == -1)
+            return;
+
+
         if (touch.phase == TouchPhase.Began)
+        {
+            if (!overUI)
+            {
+                ShowMarker(true);
+                MoveMarker(touch.position);
+            }
+            
+            //SpawnObject(touch);
+        }
+        else if (touch.phase == TouchPhase.Moved)
+        {
+            MoveMarker(touch.position);
+        }
+        else if (touch.phase == TouchPhase.Ended)
+        {
             SpawnObject(touch);
+            ShowMarker(false);
+        }
+            
+    }
+
+    private void ShowMarker(bool value)
+    {
+        _targetMarker.SetActive(value);
+    }
+
+    private void MoveMarker(Vector2 touchPosition)
+    {
+        _aRRaycastManager.Raycast(
+            screenPoint: touchPosition,
+            hitResults: _raycastHits,
+            trackableTypes: TrackableType.Planes
+            );
+        _targetMarker.transform.position = _raycastHits[0].pose.position;
     }
 
     private void SpawnObject(Touch touch)
     {
         _aRRaycastManager.Raycast(touch.position, _raycastHits, TrackableType.Planes);
-        Instantiate(_spawnedObjectPrehab, _raycastHits[0].pose.position, _spawnedObjectPrehab.transform.rotation);
+        Instantiate(_spawnedObjectPrehabs[_spawnedObjectType], _raycastHits[0].pose.position, _spawnedObjectPrehabs[_spawnedObjectType].transform.rotation);
+    }
+
+    private void InitializeDefaultScreen()
+    {
+        Debug.Log("Initialize default screen");
+        ShowMarker(false);
+    }
+
+    private void InitializeObjectSpawner()
+    {
+        Debug.Log("Initialize spawner");
+        _spawnedObjectType = -1;
+    }
+
+    public void SelectSpawnedObjectType(int objectType)
+    {
+        _spawnedObjectType = objectType;
     }
 }
